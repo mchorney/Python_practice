52fa0980dd2a25359aa1d2e01634e6ec525e819d
mchorney <m.chorney7@gmail.com>
2017-08-05 19:03:42 -0500

    added Reading From Dictionaries assignment

---
+++ readingFromDictionaries.py
@@ -0,0 +1,16 @@
+def printDictionary(dictionary):
+	for key in dictionary:
+		print 'My ',key,' is ',dictionary[key]
+
+
+
+person = {
+
+	'name': "Anna",
+	'Age': 101,
+	'Country of Birth': "United States",
+	'favorite language': "Python"
+
+}
+
+printDictionary(person)
